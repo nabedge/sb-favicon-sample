7f67027b3ce49fed05c35626ef69c3a70f7fdfed
nabedge <nabedge@gmail.com>
2015-03-26 13:26:25 +0900

    hummm...

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -4,25 +4,43 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @EnableAutoConfiguration
 @Configuration
 @ComponentScan("com.example")
 @Controller
-public class Application {
+public class Application extends WebMvcConfigurerAdapter {
 
-    @RequestMapping("/")
-    @ResponseBody
-    public String index() {
-        return "hello. get <a href=\"/favicon.ico\">favicon.ico</a>";
-    }
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		//registry.setOrder(Integer.MAX_VALUE);
+		registry.addResourceHandler("/favicon.ico").addResourceLocations("classpath:/static/musialnote.ico");
+		registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+		registry.addResourceHandler("/**/favicon.ico").addResourceLocations("classpath:/static/musicalnote.ico");
+		// .setCachePeriod(3600);
+	}
+	
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	@RequestMapping("/")
+	@ResponseBody
+	public String index() {
+		return "Hello. Get <a href=\"/favicon.ico\">favicon.ico</a> . It should be 404 Not Found .";
+	}
+
+//	@RequestMapping("/favicon.ico")
+//	@ResponseStatus(HttpStatus.NOT_FOUND)
+//	public void favicon() {
+//	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 
 }
